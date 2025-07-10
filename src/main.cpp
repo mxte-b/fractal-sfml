94fb59a77e8a94b79f862207bd7842a0e49ed592
mxte-b <182339124+mxte-b@users.noreply.github.com>
2025-07-10 13:37:48 +0200

    Project setup

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        sf::CircleShape c{100.0f};
+        c.setFillColor(sf::Color::Red);
+        window.draw(c);
+
+        window.display();
+    }
+}
